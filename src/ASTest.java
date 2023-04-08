71e7dc4dce3132c2e9d4959bb9fc3db4a6cd0d8f
Arkav Sarkar <arkav789@gmail.com>
2023-04-08 23:56:21 +0530

    First Commit message

---
+++ src/ASTest.java
@@ -0,0 +1,28 @@
+
+
+	import java.net.MalformedURLException;
+	import java.net.URL;
+
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.remote.CapabilityType;
+	import org.openqa.selenium.remote.DesiredCapabilities;
+	import org.openqa.selenium.remote.RemoteWebDriver;
+	import org.testng.annotations.Test;
+
+	public class ASTest {
+		@Test
+		public void HomepageCheck() throws MalformedURLException {
+			DesiredCapabilities caps = new DesiredCapabilities();
+			// caps.setBrowserName("chrome");
+			// caps.setPlatform(platform.);
+			// caps.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS,true);
+			caps.setCapability(CapabilityType.BROWSER_NAME, "firefox");
+			WebDriver driver = new RemoteWebDriver(new URL("http://10.0.0.6:4444"), caps);
+			driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+			System.out.println(driver.getTitle());
+			driver.close();
+		}
+	}
+
+
---
+++ src/GoogleTest.java
@@ -0,0 +1,24 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class GoogleTest {
+	@Test
+	public void HomepageCheck() throws MalformedURLException {
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setBrowserName("chrome");
+		// caps.setPlatform(platform.);
+		// caps.setCapability(CapabilityType.BROWSER_NAME, "chrome");
+		WebDriver driver = new RemoteWebDriver(new URL("http://10.0.0.6:4444"), caps);
+		driver.get("http://google.com");
+		driver.findElement(By.name("q")).sendKeys("arkav Sarkar");
+		System.out.println(driver.getTitle());
+		driver.close();
+	}
+}
